4c0c322d822c39ee62333e55ff063ec6c17ece8d
MD. RAHAT MAHAMUD <rahat3008096081@gmail.com>
2026-06-07 22:00:41 +0600

    My git

---
+++ datatype.java
@@ -0,0 +1,18 @@
+public class datatype
+{
+    public static void main(String[] args)
+    {
+        int num=10;
+        boolean b=false;
+        short s;
+        int i;
+        float f=11.5f;
+        double d;
+
+        b=true;
+        System.out.println(f);
+        System.out.printf("%d",num);
+
+    }
+
+}
--- phases.java
+++ phases.java
@@ -1,7 +1,7 @@
 public class phases 
 {
-    public static void main(String[] args)
+    public static void main(String[]args)
     {
-        System.out.println("hi");
+        System.out.println("dont forgotten");
     }
 }
