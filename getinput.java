1c5b028e3ff9b6aed174de1ad6ff788a578635b6
MD. RAHAT MAHAMUD <rahat3008096081@gmail.com>
2026-06-08 17:07:02 +0600

    Auto Commit

---
+++ getinput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class getinput
+{
+    public static void main(String[]args)
+    {
+        
+        Scanner input= new Scanner(System.in);
+       
+    String name;
+         name=input.nextLine();
+        System.out.println("name is "+name);
+
+    }
+}
